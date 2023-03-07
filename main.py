89c844a69acb28bb6d4a24c5c029716714af036d
pablo <adrilavaa@gmail.com>
2023-03-08 00:38:26 +0100

    F_Compact / uncompact finish

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from C_ColorPixelToColorGroup import colorization
 from C_ColorPixelToColorGroup import deColorization
 from D_smoothPixel import smooth
 from D_smoothPixel import unSmooth
+from F_toCompactestList import toList
+from F_toCompactestList import reverseList
 
 
 image = open("imagebis.jpg")
@@ -15,9 +17,20 @@ image = unBuild(image)
 image = simplify(image)
 #print("2", image)
 image = colorization(image)
-print("3", image)
+#print("3", image)
 image = smooth(image)
-print("4", image)
+#print("4", image)
+image = toList(image)
+print("5", image)
+image = reverseList(image)
+
+image = unSmooth(image)
+
+image = deColorization(image)
+
+image = complexify(image)
+
+image = build(image)
 
 
 
